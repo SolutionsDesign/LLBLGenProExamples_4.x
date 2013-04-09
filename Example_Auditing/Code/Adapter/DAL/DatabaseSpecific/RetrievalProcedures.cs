0a9fb7d9eddc0fb292cebca4d56ecebaa794c53f
Solutions Design bv <frans@sd.nl>
2013-04-09 14:30:36 +0200

    Ported v3 Auditing examples to LLBLGen Pro v4.

---
+++ Example_Auditing/Code/Adapter/DAL/DatabaseSpecific/RetrievalProcedures.cs
@@ -0,0 +1,28 @@
+﻿///////////////////////////////////////////////////////////////
+// This is generated code. 
+//////////////////////////////////////////////////////////////
+// Code is generated using LLBLGen Pro version: 4.0
+// Code is generated on: dinsdag 9 april 2013 13:26:19
+// Code is generated using templates: SD.TemplateBindings.SharedTemplates
+// Templates vendor: Solutions Design.
+// Templates version: 
+//////////////////////////////////////////////////////////////
+using System;
+using System.Data;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace SD.LLBLGen.Pro.Examples.Auditing.DatabaseSpecific
+{
+	/// <summary>Class which contains the static logic to execute retrieval stored procedures in the database.</summary>
+	public static partial class RetrievalProcedures
+	{
+
+
+
+
+
+		#region Included Code
+
+		#endregion
+	}
+}
