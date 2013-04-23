c7bede89378f00c5700a0dd234e22341da1a717a
Solutions Design bv <frans@sd.nl>
2013-04-23 11:25:32 +0200

    Ported Validation example to v4.0

---
+++ Example_Validation/code/SelfServicing/DAL/DaoClasses/TypedListDAO.cs
@@ -0,0 +1,30 @@
+﻿///////////////////////////////////////////////////////////////
+// This is generated code. 
+//////////////////////////////////////////////////////////////
+// Code is generated using LLBLGen Pro version: 4.0
+// Code is generated on: dinsdag 23 april 2013 10:49:48
+// Code is generated using templates: SD.TemplateBindings.SharedTemplates
+// Templates vendor: Solutions Design.
+//////////////////////////////////////////////////////////////
+using System;
+using System.Data;
+using System.Data.Common;
+using SD.LLBLGen.Pro.Examples.HelperClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace SD.LLBLGen.Pro.Examples.DaoClasses
+{
+	/// <summary>Generic DAO class for usage with Typed list classes.</summary>
+	/// <remarks>For backwards compatibility.</remarks>
+	public partial class TypedListDAO : CommonDaoBase
+	{
+		/// <summary>CTor</summary>
+		public TypedListDAO() : base()
+		{
+		}
+		
+		#region Included Code
+
+		#endregion
+	}
+}
---
+++ Example_Validation/code/SelfServicing/DAL/StoredProcedureCallerClasses/RetrievalProcedures.cs
@@ -0,0 +1,31 @@
+﻿///////////////////////////////////////////////////////////////
+// This is generated code. 
+//////////////////////////////////////////////////////////////
+// Code is generated using LLBLGen Pro version: 4.0
+// Code is generated on: dinsdag 23 april 2013 10:49:49
+// Code is generated using templates: SD.TemplateBindings.SharedTemplates
+// Templates vendor: Solutions Design.
+// Templates version: 
+//////////////////////////////////////////////////////////////
+using System;
+using System.Data;
+using System.Data.Common;
+using SD.LLBLGen.Pro.Examples.DaoClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses.SelfServicingSpecific;
+
+namespace SD.LLBLGen.Pro.Examples.StoredProcedureCallerClasses
+{
+	/// <summary>Class which contains the static logic to execute retrieval stored procedures in the database.</summary>
+	public static partial class RetrievalProcedures
+	{
+
+	
+
+
+
+		#region Included Code
+
+		#endregion
+	}
+}
