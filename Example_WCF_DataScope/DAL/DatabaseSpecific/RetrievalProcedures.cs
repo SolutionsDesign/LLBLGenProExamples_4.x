138edcf16e927112b6607ac3d80363fc56916794
Solutions Design bv <frans@sd.nl>
2013-04-23 12:20:29 +0200

    Ported WCF example to v4
    Code now uses DataScopes on the client and shows how to create a Unit of
    work from the datascope and send it to a service to be committed.

---
+++ Example_WCF_DataScope/DAL/DatabaseSpecific/RetrievalProcedures.cs
@@ -0,0 +1,28 @@
+﻿///////////////////////////////////////////////////////////////
+// This is generated code. 
+//////////////////////////////////////////////////////////////
+// Code is generated using LLBLGen Pro version: 4.0
+// Code is generated on: dinsdag 23 april 2013 12:06:13
+// Code is generated using templates: SD.TemplateBindings.SharedTemplates
+// Templates vendor: Solutions Design.
+// Templates version: 
+//////////////////////////////////////////////////////////////
+using System;
+using System.Data;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+
+namespace SD.LLBLGen.Pro.Examples.WCF.DatabaseSpecific
+{
+	/// <summary>Class which contains the static logic to execute retrieval stored procedures in the database.</summary>
+	public static partial class RetrievalProcedures
+	{
+
+
+
+
+
+		#region Included Code
+
+		#endregion
+	}
+}
